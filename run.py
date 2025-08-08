ff130bd390556a91a05a59d4e5d002c9b0508eb5
BlackKingWhitePawn <team109team@gmail.com>
2025-08-08 18:54:36 +0500

    final

--- run.py
+++ run.py
@@ -5,11 +5,17 @@ import logging
 import os
 from pathlib import Path
 from tqdm import tqdm
+import shutil
 
 logger = logging.getLogger(__name__)
 
+# ДОБАВЛЯЕМ настройки ширины терминала
+CONTEXT_SETTINGS = {
+    'max_content_width': shutil.get_terminal_size().columns
+}
 
-@click.group()
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
     logging.basicConfig(filename='log.txt', level=logging.INFO)
     pass
@@ -25,28 +31,29 @@ def data_operations(path, format):
     """
 Обрабатывает входные данные, заполняя пропуски.
 На вход принимает путь к файлу с данными или к директории.
-Если передан путь к директории то все xls/xlsx/csv в директории будут обработаны.
+Если передан путь к директории, то все xls/xlsx/csv в директории будут обработаны.
 При обработке Эксель файлов результат записывается в новый лист "Результаты".
+
 Примеры:
-\nrun data-operations  \"C:\\Users\\riabt\\Downloads\"
-\nrun data-operations \"C:\\Users\\riabt\\Download\\Telegram Desktop\" --format cd
+\nrun data-operations "C:\\Users\\riabt\\Downloads"
+\nrun data-operations "C:\\Users\\riabt\\Download\\Telegram Desktop" --format cd
 """
-    extensions = {".xls",  ".xlsx"} if (format == 'cd') else {".csv"}
+    extensions = {".xls", ".xlsx"} if (format == 'cd') else {".csv"}
     path_object = Path(path)
 
     def process_file(path: Path):
-        if (format == 'xg'):
+        if format == 'xg':
             dataOps.process_csv_dataframe(path)
-        elif (format == 'cd'):
+        elif format == 'cd':
             print(f'Обработка {path}')
             initial_df, predicted = dataOps.process_xslx_dataframe(path)
             print(f'Запись в файл {path}')
             writer.write_to_predict_sheet(path, initial_df, predicted)
 
-    if (path_object.is_dir()):
+    if path_object.is_dir():
         for filePath in tqdm([f for f in path_object.iterdir() if f.is_file() and f.suffix in extensions]):
             process_file(filePath)
-    elif (path_object.is_file() and path_object.suffix in extensions):
+    elif path_object.is_file() and path_object.suffix in extensions:
         process_file(path_object)
 
 
@@ -54,14 +61,15 @@ def data_operations(path, format):
 @click.argument('path', type=str)
 def xlsx_split(path):
     """
-Разделяет экспортированный из ЦУСАД файл на отедльные файлы с отчетами. 
-Если в экспортированном файле находится единственный отчет то сформирован будет только он.
-Итоговые файлы будут сгруппированы в директории по дорогам 
+Разделяет экспортированный из ЦУСАД файл на отдельные файлы с отчетами. 
+Если в экспортированном файле находится единственный отчет, то сформирован будет только он.
+Итоговые файлы будут сгруппированы в директории по дорогам.
+
 Примеры:
-\nrun xlsx_split  \"C:\\Users\\riabt\\Downloads\"
+\nrun xlsx_split "C:\\Users\\riabt\\Downloads"
 """
     path = Path(path)
-    if (path.is_file() and path.suffix == '.xlsx'):
+    if path.is_file() and path.suffix == '.xlsx':
         pass
 
 
